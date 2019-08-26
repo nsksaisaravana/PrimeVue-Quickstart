c42c8bcfd462571d179becdcaa23f299d7c15e0f
Saravanakumar Narayanan <saravanakumar.n@evocate.com.au>
2019-08-26 15:13:18 +1000

    inital commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h3 class="first">Basic</h3>
+    <DataTable :value="cars">
+        <Column field="vin" header="Vin"></Column>
+        <Column field="year" header="Year"></Column>
+        <Column field="brand" header="Brand"></Column>
+        <Column field="color" header="Color"></Column>
+    </DataTable>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  //@Prop() private msg!: string;
+  
+  
+  //private columns=[];
+  private cars: any;
+
+
+
+  public mounted() {
+      this.cars = {
+          data: [
+              {brand: 'Volkswagen', year: 2012, color: 'Orange', vin: 'dsad231ff'},
+          ],
+      };
+  }
+
+
+}
+</script>
